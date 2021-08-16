1bea2d1351bb6b2e1d2ee7ce7492c683bd9503c9
om <om160prakash@outlook.com>
2021-08-16 23:30:59 +0530

    table of given number by cpp program

---
+++ tablemul.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num,i;
+	cout<<"Enter the number "<<endl;
+	cin>>num;
+	
+	for(i=1;i<=10;i++)
+	{
+		cout<<num <<" X " << i<<" = "<<num*i<<endl;
+	}
+	
+	
+	return 0;
+
+}
