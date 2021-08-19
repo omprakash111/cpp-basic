6b2fdf3feddfc46d92bab11433d013a66a824085
om <om160prakash@outlook.com>
2021-08-19 23:12:28 +0530

    Prime Number second code

--- prime_Number.cpp
+++ prime_Number.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 
+#define CASE 2
 
 using  namespace std;
 
@@ -9,6 +10,7 @@ int main()
 	int num,i,count=0;
 	cout<<"Enter the number "<<endl;
 	cin>>num;
+	#if CASE==1
 	//finding factor of number and sum in sum variaable till i<num or i<= (num-1)
 	
 	for(i=1;i<=num;i++)
@@ -28,5 +30,27 @@ int main()
 	 cout<<num<<" is Prime  number "<<endl;
 	else
 	  cout<<num<<" is not Prime number "<<endl;
+	  
+	#elif CASE==2
+	
+		for(i=2;i<num;i++)
+	{
+		if(num%i==0)
+	    {
+		
+		 break;
+		 
+     	}
+		 
+		
+	}
+	
+	
+	if(num==i)
+	 cout<<num<<" is Prime  number "<<endl;
+	else
+	  cout<<num<<" is not Prime number "<<endl;
+	
+	#endif
 	return 0;
 }
