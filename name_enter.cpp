5ebf965f0792f58b94e9dcdee6a77df46aea4ae4
om <om160prakash@outlook.com>
2021-08-27 17:28:33 +0530

    cin.getline(p,40)

--- name_enter.cpp
+++ name_enter.cpp
@@ -9,12 +9,12 @@ int main()
 	
 	char p[40],p2[50];
 	cout<<"Enter the name"<<endl;
-	cin.get(p,40); //om p
+	cin.getline(p,40); //om p
 	
 	cout<<p<<endl;//om p
 	
-	cin.get(p2,30);//enter character take by p2
-	cout<<p2;//
+	cin.getline(p2,30);//aditya
+	cout<<p2;//aditya
 	
 	
 	return 0;
