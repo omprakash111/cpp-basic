07057f2b0d1e549d172ef2a1c18d946f077d9212
om <om160prakash@outlook.com>
2021-08-30 01:59:41 +0530

    solve problem

--- find.cpp
+++ find.cpp
@@ -15,15 +15,19 @@ int main()
 	cout<<s1.rfind("o")<<endl;//9  
 	
 	
-	//s1.find_first_of(char or string,from index)
+	//s1.find_first_of(char or string,start from index)
 	
 	cout<<s1.find_first_of('o')<<endl;//1
 		cout<<s1.find_first_of("re")<<endl;//5 find from left to right either r or e
+	
+	//s1.find_last_of(char or string,end to index)
+		
 			cout<<s1.find_last_of("re")<<endl;//6 find from right to left either r or e
 	
 	//cout<<s1.find_first_of()<<endl;
-	  cout<<s1.find_first_of('o',4)<<endl;//9
-		cout<<s1.find_last_of('o',4)<<endl;//1 indexing from last
+	  cout<<s1.find_first_of('o',4)<<endl;//9   
+		cout<<s1.find_last_of('o',8)<<endl;//1 
+			cout<<s1.find_last_of('o')<<endl;//9
 		
 		
 		cout<<s1.find_first_of('l')<<endl;//garbage value
