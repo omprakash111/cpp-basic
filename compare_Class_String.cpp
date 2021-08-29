e9b05cbf5b9d5136c9c67aebed39518f0e3b4870
om <om160prakash@outlook.com>
2021-08-29 23:08:58 +0530

    compare class string

---
+++ compare_Class_String.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string s1="hello";
+	string s2="Hello";
+	cout<<s1.compare(s2);//in dev c++ 1 but turbo 32
+	
+	return 0;
+}
