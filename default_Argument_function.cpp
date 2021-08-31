e8ec9e2c2d53b2bbd033d03146c53e49a4069a73
om <om160prakash@outlook.com>
2021-08-31 23:36:39 +0530

    default argument function

--- default_Argument_function.cpp
+++ default_Argument_function.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 using namespace std;
-int add(int x,int y,int z=0) //z=0 is work only when third argument not pass otherwise z is assign value by argument .And benefit in combine overloaded function
+int add(int x,int y=0,int z=0) //z=0 is work only when third argument not pass otherwise z is assign value by argument .And benefit in combine overloaded function
 {
 	return x+y+z;
 }
@@ -9,12 +9,15 @@ int add(int x,int y,int z=0) //z=0 is work only when third argument not pass oth
 
 int main()
 {
-	int c=add(4,7);
+	int c=add(4);            /* x=4,y=0,z=0  there are one argument but function defined is three argument then funtion define value by 
+	                         default argument value use so that overloaded function  in single function  call function */
+	cout<<"add 4 = "<<c<<endl;
+	 c=add(4,7);                  //x=4,y=7,z=0
 	cout<<"add 4,7 = "<<c<<endl;
-	c=add(4,7,8);
+	c=add(4,7,8);                    //x=4,y=7,z=8
 	cout<<"add 4,7,8 = "<<c<<endl;
 	
-	c=add(4,7,0);
+	c=add(4,7,0);                     //x=4,xy=7,z=0
 	cout<<"add 4,7,0 = "<<c<<endl;
 	return 0;
 }
