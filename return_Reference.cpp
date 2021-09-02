b878c28e90ed7804ae673b399db221baabd07158
om <om160prakash@outlook.com>
2021-09-02 12:44:21 +0530

    return reference

---
+++ return_Reference.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int & fun(int &a) 
+{
+	a+=1;
+ cout<<a<<endl;//11  26
+  return a; //It can return formal parameters if they are reference 
+   }
+   
+    
+int main() 
+{  
+ int x=10,i; 
+  fun(x)=25;//lvalue and this statement mean reference value and also variable fun(x)=25 or x=25.  
+  i=fun(x);
+ cout<<x<<endl<<i;// 26  26
+ return 0;
+  }
