acaffa9df434fa586447b3fefeb65fb26779f19e
om <om160prakash@outlook.com>
2021-09-02 12:54:34 +0530

    return reference commit

--- return_Reference.cpp
+++ return_Reference.cpp
@@ -11,7 +11,7 @@ int & fun(int &a)
 int main() 
 {  
  int x=10,i; 
-  fun(x)=25;//lvalue and this statement mean reference value and also variable fun(x)=25 or x=25.  
+  fun(x)=25;//lvalue and this statement mean reference value and also variable fun(x)=25 or x=25.  And function action as reference of x=25
   i=fun(x);
  cout<<x<<endl<<i;// 26  26
  return 0;
