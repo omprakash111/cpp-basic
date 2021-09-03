5f1885c708ef99eabea1ec6d6daa9670b40d22f9
om <om160prakash@outlook.com>
2021-09-03 13:54:43 +0530

    area of reactangle and create an object in heap

---
+++ create_Object_Heap.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+class Rectangle
+{
+	public :
+		int l;
+		int b;
+	int area()
+	{
+		return l*b;
+	}
+	int perimeter()
+	{
+		return 2*(l+b);
+	}
+};
+
+int main()
+{
+	Rectangle *p;
+	
+	p=new Rectangle();
+	p->l=19;
+	p->b=10;
+	cout<<p->area()<<endl<<p->perimeter();
+	
+	return 0;
+	
+}
