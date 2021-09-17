97ab19e42ea5e2d06bb7196f443326abaaa0e6f8
om <om160prakash@outlook.com>
2021-09-17 19:54:14 +0530

    virtual destructor

---
+++ practice6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string a="omdjf";
+	char b[12]="prakash";
+	cout<<*(a+2);
+}
---
+++ virtual_Destructor.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class Base
+{
+	public:
+		Base()
+		{
+			cout<<"Base constructor"<<endl;
+		}
+		
+		virtual ~Base()
+		{
+			cout<<"Base Destructor"<<endl;
+		}
+		
+};
+
+class Derived:public Base
+{ 
+public:
+	Derived()
+	{
+		cout<<"Derived constructor"<<endl;
+	
+	}
+	
+	~Derived()
+	{
+		cout<<"Derived Destructor"<<endl;
+	
+	}
+	
+	
+};
+
+int main()
+{
+	Base *p=new Derived();
+	delete p;
+}
